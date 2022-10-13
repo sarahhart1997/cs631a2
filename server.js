f3848ab5dc785ec0d578bd35657c59a0fcc5e8dd
Sarah Hart <sarah.hart.nc@gmail.com>
2022-10-12 23:45:54 -0400

    server file added

---
+++ server.js
@@ -0,0 +1,23 @@
+const mysql = require('mysql2');
+const express = require('express');
+const inputCheck = require('./utils/inputCheck');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false}));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost', 
+        // Your MySQL username,
+        user: 'root', 
+        //Your MySQL password
+        password: 'hskid1',
+        database: 'sales'
+    },
+    console.log('Connected to the election database.')
+);
